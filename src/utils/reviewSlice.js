29bf27770fbf264869ae3b4bd87bfe26fdaccbbc
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-02-26 11:02:25 +0530

    review feature added

--- src/utils/reviewSlice.js
+++ src/utils/reviewSlice.js
@@ -3,36 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const reviewSlice = createSlice({
   name: "review",
   initialState: [],
-//   reducers: {
-//     setReview: (state, action) => {
-//       return action.payload;
-//     },
-//     addReview: (state, action) => {
-//       state.push(action.payload);
-//     },
-//     deleteReview: (state, action) => {
-//       const newArray = state.filter((r) => r._id != action.payload);
-//       return newArray;
-//     },
-//   },
-reducers: {
-  getReview: (state, action) => {
-    return action.payload;
+  reducers: {
+    getReview: (state, action) => {
+      return action.payload;
+    },
+    addReview: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteReview: (state, action) => {
+      return state.filter((r) => r._id !== action.payload);
+    },
+    updateReview: (state, action) => {
+      const index = state.findIndex((r) => r._id === action.payload._id);
+      if (index !== -1) {
+        state[index] = action.payload;
+      }
+    },
   },
-  addReview: (state, action) => {
-    state.push(action.payload);
-  },
-  deleteReview: (state, action) => {
-    return state.filter(r => r._id !== action.payload);
-  },
-  updateReview: (state, action) => {
-    const index = state.findIndex(r => r._id === action.payload._id);
-    if (index !== -1) {
-      state[index] = action.payload;
-    }
-  }
-}
 });
 
-export const { getReview,addReview, deleteReview,updateReview } = reviewSlice.actions;
+export const { getReview, addReview, deleteReview, updateReview } =
+  reviewSlice.actions;
 export default reviewSlice.reducer;
